d871fdb42055acbd172f91cae70e6a14bbd9869d
Orion-P <orionpoptean@gmail.com>
2021-03-17 19:29:51 -0700

    trying to fix build/develop difference

---
+++ gatsby-ssr.js
@@ -0,0 +1,34 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+
+import React from "react"
+import AnimatedCursor from "react-animated-cursor"
+
+export default function App() {
+  return (
+    <div className="App">
+      <AnimatedCursor />
+    </div>
+  )
+}
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <div>
+      <AnimatedCursor
+        innerSize={10}
+        outerSize={10}
+        color="28, 238, 28"
+        outerAlpha={0.3}
+        innerScale={1}
+        outerScale={7}
+      />
+      {element}
+    </div>
+  )
+}
