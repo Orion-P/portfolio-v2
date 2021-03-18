c8fc347c1277590bbc3ad8b5fc8f91bcc8626bc2
Orion-P <orionpoptean@gmail.com>
2021-03-17 20:42:31 -0700

    removed animated cursor to fix css issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,28 +7,21 @@
 // You can delete this file if you're not using it
 
 import React from "react"
-import AnimatedCursor from "react-animated-cursor"
+// import AnimatedCursor from "react-animated-cursor"
 
 export default function App() {
-  return (
-    <div className="App">
-      <AnimatedCursor />
-    </div>
-  )
+  return <div className="App"></div>
 }
 
+//    //   <AnimatedCursor />
 export const wrapRootElement = ({ element }) => {
-  return (
-    <div>
-      <AnimatedCursor
-        innerSize={10}
-        outerSize={10}
-        color="28, 238, 28"
-        outerAlpha={0.3}
-        innerScale={1}
-        outerScale={7}
-      />
-      {element}
-    </div>
-  )
+  return <div>{element}</div>
 }
+// <AnimatedCursor
+// innerSize={10}
+// outerSize={10}
+// color="28, 238, 28"
+// outerAlpha={0.3}
+// innerScale={1}
+// outerScale={7}
+// />
