4a433a6f786675b3c24edd573398931fdc17ad93
Orion-P <orionpoptean@gmail.com>
2021-03-23 13:29:20 -0700

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,15 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-177075666-1", // Google Analytics / GA
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
